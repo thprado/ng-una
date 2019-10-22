820fd835453f2d0f6b77bf637874f4b9af94d095
tihprado <tih.prado@gmail.com>
2019-10-22 01:08:27 -0300

    Configuração de rota raíz para o componente Home e inclusão de div container no conteúdo.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: '',
+                loadChildren: './pages/home/home.module#HomeModule'
+            },
+            {
+                path: 'students',
+                loadChildren: './'
+            }
+        ]
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
