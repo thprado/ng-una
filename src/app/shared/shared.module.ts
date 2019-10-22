23c4bee93e1e0e31bb8bf46c667d8e3f09d0399e
tihprado <tih.prado@gmail.com>
2019-10-22 00:50:00 -0300

    Criação do conteúdo Navbar, resultado do comando: ng generate component shared/components/navbar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule
   ]
