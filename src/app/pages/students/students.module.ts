e5b40b99c0b99520507588b63ec6d06d9ede5093
tihprado <tih.prado@gmail.com>
2019-10-22 01:10:45 -0300

    Criação do nosso componente de Students, resultado do comando: ng generate component pages/students

--- src/app/pages/students/students.module.ts
+++ src/app/pages/students/students.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentsRoutingModule } from './students-routing.module';
+import { StudentsComponent } from './students.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [StudentsComponent],
   imports: [
     CommonModule,
     StudentsRoutingModule
