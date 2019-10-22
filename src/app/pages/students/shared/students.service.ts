c0b12c1f60f7b340a76fa3ce49a42b0f19a6c7fe
Thiago Prado <thiago.henrique.prado@everis.com>
2019-10-22 18:27:23 -0300

    Serviço de listagem de estudantes

--- src/app/pages/students/shared/students.service.ts
+++ src/app/pages/students/shared/students.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Student } from './student.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentsService {
 
-  constructor() { }
+  private apiPath: string = 'api/students';
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<Student[]> {
+    return this.http.get<any>(this.apiPath);
+  }
 }
